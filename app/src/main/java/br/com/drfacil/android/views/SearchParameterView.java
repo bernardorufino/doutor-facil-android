4b91bd183d3b5733f7bc80a4b18067c7a3264450
Bernardo Rufino <bermonruf@gmail.com>
2014-03-30 14:16:27 -0300

    Added search parameters and hard-coded dummy search results
    Created the search parameters view in order to change parameters in a search and
    hard-coded some search results in some dummy view also, that is not supposed to
    go to production since we agreed on using cards for the search results. Also
    created some resource files that were needed for this. Since we work very
    sparingly, I thought I should checkin this code even thought it's not production
    ready, if any problems with this talk to me. Hope at least you guys are seeing
    the commits.

---
+++ app/src/main/java/br/com/drfacil/android/views/SearchParameterView.java
@@ -0,0 +1,69 @@
+package br.com.drfacil.android.views;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+import br.com.drfacil.android.R;
+
+public class SearchParameterView extends FrameLayout {
+
+    private TextView vLabel;
+    private ImageView vIcon;
+    private TextView vValue;
+
+    public SearchParameterView(Context context) {
+        super(context);
+        init(null);
+    }
+
+    public SearchParameterView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs);
+    }
+
+    public SearchParameterView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(attrs);
+    }
+
+    private void init(AttributeSet attrs) {
+        initView();
+        if (attrs != null) {
+            initAttributes(attrs);
+        }
+    }
+
+    private void initView() {
+        inflate(getContext(), R.layout.view_search_parameter, this);
+        vLabel = (TextView) findViewById(R.id.search_parameter_label);
+        vValue = (TextView) findViewById(R.id.search_parameter_value);
+        vIcon = (ImageView) findViewById(R.id.search_parameter_icon);
+    }
+
+    private void initAttributes(AttributeSet attrs) {
+        TypedArray array = getContext().obtainStyledAttributes(attrs, R.styleable.SearchParameterView);
+        try {
+            setIcon(array.getDrawable(R.styleable.SearchParameterView_icon));
+            setLabel(array.getString(R.styleable.SearchParameterView_label));
+            setValue(array.getString(R.styleable.SearchParameterView_value));
+        } finally {
+            array.recycle();
+        }
+    }
+
+    private void setValue(String value) {
+        vValue.setText(value);
+    }
+
+    private void setLabel(String label) {
+        vLabel.setText(label);
+    }
+
+    private void setIcon(Drawable drawable) {
+        vIcon.setImageDrawable(drawable);
+    }
+}
