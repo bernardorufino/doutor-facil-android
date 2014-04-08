1ce832dd2723934f7c6f504e30f271a0ecc76a6b
Bruno Almeida <bruno.correia.almeida@gmail.com>
2014-04-08 00:02:26 -0300

    Add My Appointments view
    Summary: Add the card view for My Appointments, with hard coded data.
    
    Test Plan: Open application, go to the "Minhas consultas" tab, and make sure cards appear with a photo, the doctor name and the appointment date, time and location.
    
    Reviewers: samuel, bernardorufino
    
    Reviewed By: bernardorufino
    
    Differential Revision: http://ec2-54-186-131-188.us-west-2.compute.amazonaws.com/D13

--- app/src/main/java/br/com/drfacil/android/fragments/AppointmentsFragment.java
+++ app/src/main/java/br/com/drfacil/android/fragments/AppointmentsFragment.java
@@ -5,12 +5,29 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import br.com.drfacil.android.R;
 
 public class AppointmentsFragment extends Fragment {
 
+    private ListView mAppointmentsList;
+    private ArrayAdapter<String> mAppointmentsAdapter;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_appointments, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mAppointmentsList = (ListView) getView().findViewById(R.id.appointments_list);
+
+        /*TODO: Unhardcode this */
+        mAppointmentsAdapter = new ArrayAdapter<>(getActivity(), R.layout.appointments_item, R.id.appointment_doctor_name);
+        mAppointmentsAdapter.addAll("Dr. Pedro Albuquerque", "Dr. Rudolph Green");
+
+        mAppointmentsList.setAdapter(mAppointmentsAdapter);
     }
 }
